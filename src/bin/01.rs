0adb5c25dfd60e8416769035d8b77fcef26d674e
sebous <sebous@gmail.com>
2022-12-01 09:38:28 +0100

    day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,36 @@
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    input
+        .split("\n\n")
+        .map(|inv| {
+            let items = inv
+                .lines()
+                .map(|val| val.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
+            items.iter().sum::<u32>()
+        })
+        .max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let res = input
+        .split("\n\n")
+        .map(|inv| {
+            let items = inv
+                .lines()
+                .map(|val| val.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
+            items.iter().sum::<u32>()
+        })
+        .sorted_by(|a, b| Ord::cmp(b, a))
+        .take(3)
+        .sum::<u32>();
+    Some(res)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
