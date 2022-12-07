1c94726b287fb85cbee9fafb2c885c1cebf9b8d5
sebous <sebous@gmail.com>
2022-12-07 13:36:46 +0100

    d7 cleanup

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 fn build_tree(input: &str) -> HashMap<String, u64> {
     let mut tree: HashMap<String, u64> = HashMap::new();
     let mut pwd: Vec<String> = vec![];
+
     for line in input.lines() {
         match &line[..4] {
             "$ cd" => match &line[5..] {
@@ -10,26 +11,18 @@ fn build_tree(input: &str) -> HashMap<String, u64> {
                     pwd.pop();
                 }
                 x => {
-                    pwd.push(if x == "/" {
-                        x.to_owned()
-                    } else {
-                        x.to_owned() + "/"
-                    });
-                    tree.entry(pwd.join("")).or_insert(0);
+                    pwd.push(x.to_owned() + if x == "/" { "" } else { "/" });
+                    tree.insert(pwd.join(""), 0);
                 }
             },
-            "$ ls" => continue,
+            "$ ls" | "dir " => continue,
             _ => {
                 let (first, _) = line.split_once(" ").unwrap();
-                let num = first.parse::<u64>();
-                match num {
-                    Ok(num) => {
-                        tree.iter_mut()
-                            .filter(|(path, _)| pwd.join("").starts_with(*path))
-                            .for_each(|(_, value)| *value += num);
-                    }
-                    _ => continue,
-                }
+                let num: u64 = first.parse().unwrap();
+                let current_path = pwd.join("");
+                tree.iter_mut()
+                    .filter(|(path, _)| current_path.starts_with(*path))
+                    .for_each(|(_, value)| *value += num);
             }
         }
     }
@@ -38,14 +31,15 @@ fn build_tree(input: &str) -> HashMap<String, u64> {
 
 pub fn part_one(input: &str) -> Option<u64> {
     let tree = build_tree(input);
-    Some(tree.values().filter(|val| *val <= &(100000 as u64)).sum())
+    let total_size = tree.values().filter(|val| *val <= &(100000 as u64)).sum();
+    Some(total_size)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let tree = build_tree(input);
     let total_space: u64 = 70000000;
     let needed_space: u64 = 30000000;
-    let used_space = tree.get("/").unwrap().to_owned();
+    let used_space = tree.get("/").unwrap();
     tree.values()
         .filter(|val| (used_space - *val) <= total_space - needed_space)
         .min()
