8a9d02473fd41338701219435680778f8928904e
sebous <sebous@gmail.com>
2022-12-09 15:10:32 +0100

    day 9 oof

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -27,13 +27,34 @@ enum Axis {
     Y,
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut visited_coords = HashSet::from([(0, 0)]);
+fn print_tails(tails: &Vec<(i32, i32)>) {
+    for y in (-10..20).rev() {
+        let line: String = (-20..30)
+            .map(|x| {
+                tails
+                    .iter()
+                    .position(|tail| *tail == (x, y))
+                    .map(|num| (num + 1).to_string())
+                    .or_else(|| {
+                        if (x, y) == (0, 0) {
+                            Some("s".to_string())
+                        } else {
+                            Some(".".to_string())
+                        }
+                    })
+            })
+            .filter_map(|s| s)
+            .collect();
+        println!("{line}");
+    }
+}
+
+fn solve(input: &str, tail_size: u8) -> HashSet<(i32, i32)> {
+    let mut visited_coords = HashSet::new();
     let mut head_pos = (0, 0);
-    let mut tail_pos = (0, 0);
+    let mut tails = (0..tail_size).map(|_| (0, 0)).collect_vec();
 
     for line in input.lines() {
-        // println!("--- {} ---", &line);
         let (dir, val) = line.split_once(" ").unwrap();
         let val: i32 = val.parse().unwrap();
 
@@ -46,37 +67,51 @@ pub fn part_one(input: &str) -> Option<u32> {
         };
 
         let head_path = create_path(&head_pos, &next_head_pos);
-        // println!("head path: {:?}", &head_path);
 
         for pos in head_path {
-            head_pos = pos;
-            // println!("H {:?}", head_pos);
-            if !should_move(&head_pos, &tail_pos) {
-                // println!("T {:?}", tail_pos);
-                continue;
-            }
-            let (x, y) = tail_pos;
+            for i in 0..tails.len() {
+                let mut prev_pos = pos;
+                if i == 0 {
+                    head_pos = pos;
+                } else {
+                    prev_pos = tails[i - 1];
+                }
 
-            let x_move = if head_pos.0 > x { 1 } else { -1 };
-            let y_move = if head_pos.1 > y { 1 } else { -1 };
-            if head_pos.1 == y {
-                tail_pos = (x + x_move, y);
-            } else if head_pos.0 == x {
-                tail_pos = (x, y + y_move);
-            } else {
-                tail_pos = (x + x_move, y + y_move);
-            }
-            visited_coords.insert(tail_pos);
+                let tail_pos = tails[i];
 
-            // println!("T {:?}", tail_pos);
+                if !should_move(&prev_pos, &tail_pos) {
+                    continue;
+                }
+                let (x, y) = tail_pos;
+
+                let x_move = if prev_pos.0 > x { 1 } else { -1 };
+                let y_move = if prev_pos.1 > y { 1 } else { -1 };
+                if prev_pos.1 == y {
+                    tails[i] = (x + x_move, y);
+                } else if prev_pos.0 == x {
+                    tails[i] = (x, y + y_move);
+                } else {
+                    tails[i] = (x + x_move, y + y_move);
+                }
+                if i == tails.len() - 1 {
+                    visited_coords.insert(tail_pos);
+                }
+            }
         }
+        // println!("--- {} ---", &line);
+        // print_tails(&tails);
     }
+    visited_coords
+}
 
-    Some(visited_coords.len() as u32)
+pub fn part_one(input: &str) -> Option<u32> {
+    let visited_coords = solve(input, 1);
+    Some((visited_coords.len() + 1) as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let visited_coords = solve(input, 9);
+    Some((visited_coords.len() + 1) as u32)
 }
 
 fn main() {
@@ -98,6 +133,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(36));
     }
 }
